556dd67b5fe16992b33a1f167d46984280a11abd
Sarah Bunker <ChemEnCoder@gmail.com>
2022-11-30 13:25:42 -0700

    moving filter to own component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
-import { createNote, toggleImportanceOf } from './reducers/noteReducer'
-import { useSelector, useDispatch } from 'react-redux'
-import NewNote from './components/NewNote'
 import Notes from './components/Notes'
+import NewNote from './components/NewNote'
+import VisibilityFilter from './components/VisibilityFilter'
 
 const App = () => {
-  const filterSelected = (value) => {
-    console.log(value)
-  }
-
   return (
     <div>
       <NewNote />
-      <div>
-        all          <input type="radio" name="filter"
-          onChange={() => filterSelected('ALL')} />
-        important    <input type="radio" name="filter"
-          onChange={() => filterSelected('IMPORTANT')} />
-        nonimportant <input type="radio" name="filter"
-          onChange={() => filterSelected('NONIMPORTANT')} />
-      </div>
+      <VisibilityFilter />
       <Notes />
     </div>
   )
