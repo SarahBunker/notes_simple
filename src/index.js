0d35010782cdf81f173d7423a4f44702c910f263
Sarah Bunker <ChemEnCoder@gmail.com>
2022-11-30 11:40:50 -0700

    separating creator functions

--- src/index.js
+++ src/index.js
@@ -13,6 +13,24 @@ const noteReducer = (state = [], action) => {
 
 const store = createStore(noteReducer)
 
+const createNote = (content) => {
+  return {
+    type: 'NEW_NOTE',
+    data: {
+      content,
+      important: false,
+      id: generateId()
+    }
+  }
+}
+
+const toggleImportanceOf = (id) => {
+  return {
+    type: 'TOGGLE_IMPORTANCE',
+    data: { id }
+  }
+}
+
 store.dispatch({
   type: 'NEW_NOTE',
   data: {
@@ -31,16 +49,38 @@ store.dispatch({
   }
 })
 
+const generateId = () =>
+  Number((Math.random() * 1000000).toFixed(0))
+
 const App = () => {
-  return(
+  const addNote = (event) => {
+    event.preventDefault()
+    const content = event.target.note.value
+    event.target.note.value = ''
+    store.dispatch(createNote(content))
+
+  }
+
+  const toggleImportance = (id) => {
+    store.dispatch(toggleImportanceOf(id))
+  }
+
+  return (
     <div>
+      <form onSubmit={addNote}>
+        <input name="note" />
+        <button type="submit">add</button>
+      </form>
       <ul>
-        {store.getState().map(note=>
-          <li key={note.id}>
+        {store.getState().map(note =>
+          <li
+            key={note.id}
+            onClick={() => toggleImportance(note.id)}
+          >
             {note.content} <strong>{note.important ? 'important' : ''}</strong>
           </li>
         )}
-        </ul>
+      </ul>
     </div>
   )
 }
