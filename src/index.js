614eb0f410375cba523ab71a42c424f8c403b25e
Sarah Bunker <ChemEnCoder@gmail.com>
2022-11-30 13:16:47 -0700

    more components, intiializing state

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-
-import App from './App'
-
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
+import App from './App'
 import noteReducer from './reducers/noteReducer'
 
 const store = createStore(noteReducer)
